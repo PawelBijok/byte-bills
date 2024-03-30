41f900c680e13d93f5158e301492e0b72fd4caa0
Paweł Bijok <bijok.pawel@gmail.com>
2024-03-30 17:19:46 +0100

    finish authentication flow

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,79 +1,126 @@
-import React, { useState } from "react"
-import { Alert, StyleSheet, View } from "react-native"
-import { supabase } from "../lib/supabase"
-import { AppButton } from "./ui/buttons/AppButton"
-import { AppInput } from "./ui/inputs/AppInput"
+import React, { useState } from "react";
+import { Alert, StyleSheet, View } from "react-native";
+import { supabase } from "../lib/supabase";
+import { AppButton } from "./ui/buttons/AppButton";
+import { AppInput, AppInputStatus } from "./ui/inputs/AppInput";
+
+const validateEmail = (email: string): boolean => {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+};
+
+const validatePassword = (password: string): boolean => {
+  return password.length >= 5;
+};
+
+type AuthState = {
+  email: string;
+  password: string;
+  loading: boolean;
+  emailStatus: AppInputStatus;
+  passwordStatus: AppInputStatus;
+  readonly isValid: () => boolean;
+};
 
 export default function Auth() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [loading, setLoading] = useState(false)
+  const initialState: AuthState = {
+    email: "",
+    password: "",
+    loading: false,
+    emailStatus: "initial",
+    passwordStatus: "initial",
+    isValid: function (): boolean {
+      return validateEmail(this.email) && validatePassword(this.password);
+    },
+  };
+  const [state, setState] = useState<AuthState>(initialState);
 
   async function signInWithEmail() {
-    setLoading(true)
+    if (!state.isValid()) {
+      return;
+    }
+    setState((state) => ({ ...state, loading: true }));
     const { error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    })
-
-    if (error) Alert.alert(error.message)
-    setLoading(false)
+      email: state.email,
+      password: state.password,
+    });
+    if (error) Alert.alert(error.message);
+    setState((state) => ({ ...state, loading: false }));
   }
 
   async function signUpWithEmail() {
-    setLoading(true)
+    if (!state.isValid()) {
+      return;
+    }
+    setState((state) => ({ ...state, loading: true }));
     const {
       data: { session },
       error,
     } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    })
-
-    if (error) Alert.alert(error.message)
-    if (!session) Alert.alert("Please check your inbox for email verification!")
-    setLoading(false)
+      email: state.email,
+      password: state.password,
+    });
+    if (error) Alert.alert(error.message);
+    if (!session)
+      Alert.alert("Please check your inbox for email verification!");
+    setState((state) => ({ ...state, loading: false }));
   }
 
   return (
     <View style={styles.container}>
       <View style={[styles.verticallySpaced, styles.mt20]}>
         <AppInput
-          value={email}
+          value={state.email}
           placeholder="unique email"
-          onChangeText={(text) => setEmail(text)}
+          autoCapitalize="none"
+          onChangeText={(text) =>
+            setState((state) => ({
+              ...state,
+              email: text,
+              emailStatus: validateEmail(text) ? "ok" : "error",
+            }))
+          }
+          status={state.emailStatus}
           secureTextEntry={false}
           label="Email"
           keyboardType="email-address"
+          errorText="Invalid email address."
         />
       </View>
-      {/* <View style={[styles.verticallySpaced, styles.mt20]}>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
         <AppInput
-          value={password}
+          value={state.password}
           placeholder="super secret password"
-          onChangeText={(text) => setPassword(text)}
+          autoCapitalize="none"
+          onChangeText={(text) =>
+            setState((state) => ({
+              ...state,
+              password: text,
+              passwordStatus: validatePassword(text) ? "ok" : "error",
+            }))
+          }
           secureTextEntry={true}
           label="Password"
           keyboardType="visible-password"
+          status={state.passwordStatus}
+          errorText="Password must be at least 5 characters long."
         />
-      </View> */}
+      </View>
+      <View style={[styles.verticallySpaced, styles.mt20]}></View>
+      <AppButton
+        title={"Sign in"}
+        onPress={signInWithEmail}
+        loading={state.loading}
+      />
       <View style={[styles.verticallySpaced, styles.mt20]}>
         <AppButton
           title="Sign up"
-          onPress={() => signUpWithEmail()}
-          loading={loading}
+          onPress={signUpWithEmail}
+          loading={state.loading}
         ></AppButton>
       </View>
-      <View style={[styles.verticallySpaced]}></View>
-      <AppButton
-        title={"Sign up"}
-        onPress={() => {
-          setLoading((loading) => !loading)
-        }}
-        loading={loading}
-      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -89,4 +136,4 @@ const styles = StyleSheet.create({
   mt20: {
     marginTop: 20,
   },
-})
+});
--- components/ui/inputs/AppInput.tsx
+++ components/ui/inputs/AppInput.tsx
@@ -4,33 +4,36 @@ import {
   Text,
   TextInput,
   View,
-} from "react-native"
-import { errorColor, isDark, okColor, onBgColor } from "../../../lib/themes"
-import { DashedSpacer } from "../spacers/DashedSpacer"
+} from "react-native";
+import { errorColor, isDark, okColor, onBgColor } from "../../../lib/themes";
+import { DashedSpacer } from "../spacers/DashedSpacer";
 
-export type AppInputStatus = "initial" | "ok" | "error"
+export type AppInputStatus = "initial" | "ok" | "error";
+
 type AppInputProps = {
-  value?: string
-  onChangeText?: (value: string) => void
-  placeholder?: string
-  secureTextEntry?: boolean
-  label?: string
-  keyboardType?: KeyboardTypeOptions
-  status?: AppInputStatus
-  errorText?: string
-}
+  value?: string;
+  onChangeText?: (value: string) => void;
+  placeholder?: string;
+  secureTextEntry?: boolean;
+  label?: string;
+  keyboardType?: KeyboardTypeOptions;
+  status?: AppInputStatus;
+  errorText?: string;
+  autoCapitalize?: "none" | "sentences" | "words" | "characters";
+};
+
 export const AppInput = (props: AppInputProps) => {
-  let color = onBgColor()
+  let color = onBgColor();
   switch (props.status) {
     case "ok":
-      color = okColor()
-      break
+      color = okColor();
+      break;
     case "error":
-      color = errorColor()
-      break
+      color = errorColor();
+      break;
     case "initial" || undefined:
-      color = onBgColor()
-      break
+      color = onBgColor();
+      break;
   }
   return (
     <View style={styles.input}>
@@ -43,6 +46,7 @@ export const AppInput = (props: AppInputProps) => {
           paddingTop: 10,
           color: color,
         }}
+        autoCapitalize={props.autoCapitalize}
         placeholderTextColor={isDark() ? "#999" : "#888"}
         keyboardType={props.keyboardType}
         value={props.value}
@@ -51,10 +55,13 @@ export const AppInput = (props: AppInputProps) => {
         secureTextEntry={props.secureTextEntry}
       />
       <DashedSpacer color={color} />
+      {props.errorText != null && props.status == "error" ? (
+        <Text style={{ color: color }}>{props.errorText}</Text>
+      ) : null}
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   input: {},
-})
+});
